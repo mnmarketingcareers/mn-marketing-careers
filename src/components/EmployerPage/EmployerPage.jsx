7df8bf6fad0b81fa52acef7b1f3c8edc51d1bbb2
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-11-07 02:46:26 -0600

    Employer page progress.

--- src/components/EmployerPage/EmployerPage.jsx
+++ src/components/EmployerPage/EmployerPage.jsx
@@ -7,6 +7,10 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Grid';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
 
 import './EmployerPage.css';
 
@@ -15,11 +19,17 @@ function EmployerPage() {
     const history = useHistory();
     const dispatch = useDispatch();
 
-    const [jobPostingsTable, setJobPostingsTable] = useState({ company: '', available_role: '', application_link: '', description: '', job_city: '', job_state: '', remote: '' });
+    const [editMode, setEditMode] = useState(true);
+
+    const [jobPostingsTable, setJobPostingsTable] = useState({ company: '', available_role: '', application_link: '', description: '', job_city: '', job_state: '', remote: '', share_contact: '' });
 
     const submitEmployerJob = (event) => {
         console.log('what is jobPostingsTable', jobPostingsTable)
 
+    }
+
+    const changeState = () => {
+        setEditMode(!editMode)
     }
 
     const setValues = (event) => {
@@ -46,6 +56,13 @@ function EmployerPage() {
                 break;
             case 'PrivateSwitchBase-input css-1m9pwf3':
                 setJobPostingsTable({ ...jobPostingsTable, remote: event.target.value })
+                setEditMode(true);
+                break;
+            case 'other':
+                setJobPostingsTable({ ...jobPostingsTable, remote: event.target.value })
+                break;
+            case 'test':
+                setJobPostingsTable({ ...jobPostingsTable, share_contact: event.target.value })
                 break;
         }
     }
@@ -53,56 +70,105 @@ function EmployerPage() {
     return (
         <>
             <form className="employer-form" onSubmit={submitEmployerJob}>
-                <input
-                    type="text"
-                    placeholder="company"
-                    className="company"
-                    onChange={setValues}
-                    value={jobPostingsTable.company} ></input>
-                <input
-                    type="text"
-                    placeholder="Title"
-                    className="title"
-                    onChange={setValues}
-                    value={jobPostingsTable.available_role} ></input>
-                <input
-                    type="text"
-                    placeholder="Link"
-                    className="application-link"
-                    onChange={setValues}
-                    value={jobPostingsTable.application_link} ></input>
-                <input
-                    type="text"
-                    placeholder="Description"
-                    className="description"
-                    onChange={setValues}
-                    value={jobPostingsTable.description} ></input>
-                <input
-                    type="text"
-                    placeholder="City"
-                    className="city"
-                    onChange={setValues}
-                    value={jobPostingsTable.job_city} ></input>
-                <input
-                    type="text"
-                    placeholder="State"
-                    maxLength="2"
-                    className="state"
-                    onChange={setValues}
-                    value={jobPostingsTable.job_state} ></input>
-
-                <FormControl component="fieldset">
-                    <FormLabel component="legend">Is this job remote?</FormLabel>
-                    <RadioGroup
-                        aria-label="gender"
-                        defaultValue="other"
-                        name="radio-buttons-group"
-                    >
-                        <FormControlLabel value="yes" onClick={setValues} control={<Radio />} label="yes" />
-                        <FormControlLabel value="no" onClick={setValues} control={<Radio />} label="no" />
-                        <FormControlLabel value="other" onClick={setValues} control={<Radio />} label="Other" />
-                    </RadioGroup>
-                </FormControl>
+                <Grid container spacing={2}>
+                    <Grid item xs={8}>
+                        <Card>
+                            <CardHeader title="Company Name" />
+                            <input
+                                type="text"
+                                placeholder="company"
+                                className="company"
+                                onChange={setValues}
+                                value={jobPostingsTable.company} ></input>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Card>
+                            <CardHeader title="Title of Position Available" />
+                            <input
+                                type="text"
+                                placeholder="Title"
+                                className="title"
+                                onChange={setValues}
+                                value={jobPostingsTable.available_role} ></input>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Card>
+                            <CardHeader title="Link to Job Post Online" />
+                            <input
+                                type="text"
+                                placeholder="Link"
+                                className="application-link"
+                                onChange={setValues}
+                                value={jobPostingsTable.application_link} ></input>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Card>
+                            <CardHeader title="Description" />
+                            <input
+                                type="text"
+                                placeholder="Description"
+                                className="description"
+                                onChange={setValues}
+                                value={jobPostingsTable.description} ></input>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Card>
+                            <CardHeader title="City" />
+                            <input
+                                type="text"
+                                placeholder="City"
+                                className="city"
+                                onChange={setValues}
+                                value={jobPostingsTable.job_city} ></input>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Card>
+                            <CardHeader title="State" />
+                            <input
+                                type="text"
+                                placeholder="State"
+                                maxLength="2"
+                                className="state"
+                                onChange={setValues}
+                                value={jobPostingsTable.job_state} ></input>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Card>
+                            <CardHeader title="Is this job remote?" />
+                            <FormControl component="fieldset">
+                                <RadioGroup
+                                    aria-label="Is this job remote?"
+                                    name="radio-buttons-group"
+                                >
+                                    <FormControlLabel value="yes" onClick={setValues} control={<Radio />} label="yes" />
+                                    <FormControlLabel value="no" onClick={setValues} control={<Radio />} label="no" />
+                                    <FormControlLabel value="other" onClick={changeState} control={<Radio />} label="Other" />
+                                    {editMode ? <p></p> : <p><input className="other" placeholder="details" onChange={setValues} ></input></p>}
+                                </RadioGroup>
+                            </FormControl>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={8}>
+                        <Card>
+                            <CardHeader title="Can we share a contact person?" />
+                            <FormControl component="fieldset">
+                                <RadioGroup
+                                    aria-label="Can we share a contact person?"
+                                    name="radio-buttons-group-contacts"
+                                >
+                                    <FormControlLabel inputprops={{ className: "test" }} value="yes" onClick={setValues} control={<Radio />} label="yes" />
+                                    <FormControlLabel inputprops={{ className: "test" }} value="no" onClick={setValues} control={<Radio />} label="no" />
+                                </RadioGroup>
+                            </FormControl>
+                        </Card>
+                    </Grid>
+                </Grid>
 
 
                 <input className="submit-employer-form-button" type='submit' value='Submit' />
