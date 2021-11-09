6d6a3d059015f538d6fee30f07191358031b0355
Danny Mussie <dannymussie7@gmail.com>
2021-11-09 16:01:06 -0600

    router.post request written

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -3,7 +3,24 @@ const axios = require('axios');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-
+// Handles POST request with new job seeker feedback
+router.post('/api/feedback', (req, res) => {
+    const reason = req.body.reason;
+    const message = req.body.message;
+    const archived = req.body.archived;
+    const date_recieved = req.body.date_recieved;
+    // the query that's responsible for inserting user feedback into the feedback database table
+    const queryText = `INSERT INTO "feedback" (reason, message, archived, date_received)
+    VALUES ($1, $2, $3, $4) RETURNING id;`;
+    // this pools the query text and datafields and sends the data on to the database 
+    pool
+        .query(queryText, [reason, message, archived, date_recieved])
+        .then(() => res.sendStatus(201))
+        .catch((err) => {
+            console.log('feedback POST failed: ', err);
+            res.sendStatus(500);
+        })
+})
 
 
 module.exports = router;
