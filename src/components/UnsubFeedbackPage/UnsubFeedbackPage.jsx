ec4fd21043d348c5120874d8888cd8d4b86e1b0e
Danny Mussie <dannymussie7@gmail.com>
2021-11-10 09:17:53 -0600

    unsub feedback page commit

---
+++ src/components/UnsubFeedbackPage/UnsubFeedbackPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
+
+function UnsubFeedbackPage() {
+
+    // in this return I want a selection of reasons as radio/check buttons
+    // conditional rendering to the DOM
+    // if a job seeker selects other 
+    // there needs to be an other text field for comments
+    // send results to database on click submit
+    // navigate back to main page option needs to be available
+    
+    return (
+        <div>
+            <h2>Unsubcribing? Let Us Know Why!</h2>
+        </div>
+        
+    )
+}
+
+export default UnsubFeedbackPage;
