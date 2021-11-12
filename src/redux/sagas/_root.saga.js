e85e9f0d940e5f094c41272b27c563adf1f7059d
Chris "Mo" Mochinski <cmochinski@gmail.com>
2021-11-12 00:27:14 -0600

    put sub toggle operational

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -5,6 +5,7 @@ import userSaga from './user.saga';
 import employerSaga from './employer.saga';
 import getSubsSaga from './getSubs.saga';
 import addSubscriberSaga from './addSubscriber.saga';
+import toggleSubscriberStatus from './modifySubStatus.saga';
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -20,6 +21,7 @@ export default function* rootSaga() {
     userSaga(),
     employerSaga(),
     getSubsSaga(), //get from API
-    addSubscriberSaga() //post to API
+    addSubscriberSaga(), //post to API
+    toggleSubscriberStatus() //PUT sub status to API
   ]);
 }
