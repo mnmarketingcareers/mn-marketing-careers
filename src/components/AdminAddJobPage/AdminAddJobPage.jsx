9b97ef6be1a50ea2177d60872f117bd6d6402e0a
Danny Mussie <dannymussie7@gmail.com>
2021-11-13 13:31:13 -0600

    changed client side layout to just include a job input form, and starting a dispatch payload in handleSubmit

--- src/components/AdminAddJobPage/AdminAddJobPage.jsx
+++ src/components/AdminAddJobPage/AdminAddJobPage.jsx
@@ -7,6 +7,7 @@ import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import { useHistory } from "react-router";
 import useStyles from "../Styles/Styles";
 
@@ -14,24 +15,8 @@ const AdminAddJobPage = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
     const history = useHistory();
-    const rows = useSelector((store) => store.setJobsReducer);
-    const columns = [
-        { field: 'id', headerName: 'id', width: 150 },
-        { field: 'available_role', headerName: 'available role', width: 150 },
-        { field: 'description', headerName: 'description', width: 150 },
-        { field: 'application_link', headerName: 'link', width: 150 },
-        { field: 'job_city', headerName: 'city', width: 150 },
-        { field: 'job_state', headerName: 'State', width: 150 },
-        { field: 'remote', headerName: 'Remote?', width: 150 },
-        { field: 'date_posted', headerName: 'date', width: 150 },
-        { field: 'hiring_contact_email', headerName: 'Contact Email', width: 150 },
-        { field: 'hiring_contact_name', headerName: 'Contact Came', width: 150 },
-        { field: 'title', headerName: 'Title', width: 150 },
-        { field: 'phone', headerName: 'Phone', width: 150 },
-        { field: 'company_name', headerName: 'company', width: 150 },
-        { field: 'phone', headerName: 'Phone', width: 150, },
-    ];
-
+    // const rows = useSelector((store) => store.setJobsReducer);
+    const [companyName, setCompanyName] = useState('');
     const [availableRole, setAvailableRole] = useState('');
     const [description, setDescription] = useState('');
     const [applicationLink, setApplicationLink] = useState('');
@@ -39,13 +24,16 @@ const AdminAddJobPage = () => {
     const [jobState, setJobState] = useState('');
     const [remote, setRemote] = useState('no');
     const [shareContact, setShareContact] = useState(false);
+    const [status, setStatus] = useState('APPROVED');
 
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log('the event is', event);
         dispatch({
             type: 'FETCH_MAIN_JOBS',
-            payload: rows
+            payload: {
+                company_name: companyName
+            }
         })
         alert('Successfully added job!')
     }
@@ -111,107 +99,9 @@ const AdminAddJobPage = () => {
               <Button onClick={handleSubmit} className={classes.adminSubmitButton} variant="contained" type="submit">Submit</Button>
             </form>
           </div>
-
-          <div className="gridR">
-            <Typography
-              variant="h4"
-              style={{ margin: "20px", textDecoration: "underline" }}
-            >
-              Full Jobs List:
-            </Typography>
-
-            <Paper elevation={8} className="adminPaper">
-              <TableContainer sx={{ maxHeight: 470 }}>
-              <Table className="tableMain" stickyHeader aria-label="sticky table">
-                <TableHead className={classes.tableHeader}>
-                  <TableRow>
-                    <TableCell
-                      className={classes.tableHeaderCell}
-                    >
-                      Role
-                    </TableCell>
-                    <TableCell
-                      className={classes.tableHeaderCell}
-                    >
-                      Description
-                    </TableCell>
-                    <TableCell
-                      className={classes.tableHeaderCell}
-                    >
-                      Job Link
-                       </TableCell>
-                    <TableCell
-                      className={classes.tableHeaderCell}
-                    >
-                      City
-                    </TableCell>
-                    <TableCell
-                      className={classes.tableHeaderCell}
-                    >
-                      State
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                {/* <TableBody className="adminTableBody">
-                  {rows.length > 0 ? (
-                    rows[0].map((item) => (
-                      <TableRow key={item.id} className={classes.tableBodyRow}>
-                        <TableCell
-                          style={{
-                          
-                            fontFamily: "Lato",
-                            textAlign: "center",
-                            fontSize: "16px",
-                          }}
-                        >
-                          {item.available_role}
-                        </TableCell>
-                        <TableCell
-                          style={{
-                            fontFamily: "Lato",
-                            textAlign: "center",
-                            fontSize: "16px",
-                          }}
-                        >
-                          {item.description}
-                        </TableCell>
-
-
-                        <TableCell
-                          style={{
-                            fontFamily: "Lato",
-                            textAlign: "center",
-                            fontSize: "16px",
-                          }}
-                        >
-                          {item.application_link}
-                        </TableCell>
-
-
-                        <TableCell 
-                          style={{
-                            fontFamily: "Lato",
-                            textAlign: "center",
-                            fontSize: "16px",
-                          }}
-                        >
-                          {item.job_city}
-                        </TableCell>
-                        <IconButton onClick={() => toggleSubStatus(item.status, item.contact_id)}><ToggleOffIcon/></IconButton>
-                      </TableRow>
-                    ))
-                  ) : (
-                    <img src="./images/Pendulum.gif" />
-                  )}
-                </TableBody> */}
-              </Table>
-              </TableContainer>
-            </Paper>
-            <Typography></Typography>
-          </div>
         </div>
-      </Container>
-    </div>
+      </Container> 
+        </div>
     );
 };
 
