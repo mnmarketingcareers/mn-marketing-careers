494f33c726d9d3fb1b953a67f554350c7c4308f1
Danny Mussie <dannymussie7@gmail.com>
2021-11-15 11:16:37 -0600

    started on Job Postings Issue Page

---
+++ src/components/JobPostingIssuesPage/JobPostingIssuesPage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useHistory } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import { TextField } from "@mui/material";
+import "./UnsubFeedbackPage.css";
+import FormGroup from "@mui/material/FormGroup";
+import axios from "axios";
+
+function JobPostingIssuesPage() {
+
+    return (
+
+    );
+};
+
+export default JobPostingIssuesPage;
