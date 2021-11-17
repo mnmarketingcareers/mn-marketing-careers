7430bc30859ce469e902f75a5a8c92653eac4c06
Danny Mussie <dannymussie7@gmail.com>
2021-11-16 20:55:56 -0600

    created router.get in job issues router to get all job issue data from the database

--- server/routes/job_issues.router.js
+++ server/routes/job_issues.router.js
@@ -24,4 +24,20 @@ router.post('/', (req, res) => {
         })
 })
 
+// Handles GET request which fetches data in the issues table, which involves a JOIN with the job_postings table
+router.get('/', (req, res) => {
+    console.log('In GET for all job issues');
+    const queryText = `SELECT "i"."id", "comment", "issue_type", "is_resolved", "issues_email"   
+    FROM "issues" AS "i"
+    JOIN "job_postings" AS "jp" ON "jp"."id" = "i"."job_posting_id";
+    `;
+    pool.query(queryText).then((results) => {
+        console.log('Job issues to send', results.rows);
+        res.send(results.rows);
+    }).catch(error => {
+        console.log('ERROR in GET all job issues', error);
+        res.sendStatus(500);
+    })
+})
+
 module.exports = router;
