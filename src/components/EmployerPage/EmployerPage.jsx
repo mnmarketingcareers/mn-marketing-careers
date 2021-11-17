fda5c88b07e73798419851385f0e05998ac67cc7
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-11-17 12:14:55 -0600

    removed a whole lotta console.logs, and something that was causing a warning message in ShareOurApp.jsx

--- src/components/EmployerPage/EmployerPage.jsx
+++ src/components/EmployerPage/EmployerPage.jsx
@@ -83,7 +83,6 @@ function EmployerPage() {
 
     const submitEmployerJob = (event) => {
         event.preventDefault();
-        console.log('what is jobPostingsTable', jobPostingsTable)
         dispatch({
             type: 'NEW_EMPLOYER_JOB_POST',
             payload: jobPostingsTable
@@ -129,16 +128,12 @@ function EmployerPage() {
 
 
     const setValues = (propertyName) => (event) => {
-        console.log('what is propertyName', propertyName);
-        console.log('what is event.target.value', event.target.value);
         setJobPostingsTable({ ...jobPostingsTable, [propertyName]: event.target.value });
     };
 
 
     // Two functions for the "Can we share a contact person?"
     const shareContact = (event) => {
-        console.log('what is event', event.target.value)
-        console.log('in yes')
         setJobPostingsTable({ ...jobPostingsTable, share_contact: true });
         changeContactView();
     };
