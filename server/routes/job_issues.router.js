077aedd1f777fa95035b505f7ebb53611dbc2d06
Danny Mussie <dannymussie7@gmail.com>
2021-11-16 20:11:24 -0600

    edited query in router.post in job issues router

--- server/routes/job_issues.router.js
+++ server/routes/job_issues.router.js
@@ -5,15 +5,18 @@ const router = express.Router();
 // Handles POST request with new job issues raised by job seekers
 router.post('/', (req, res) => {
     console.log('req.body is', req.body);
+    const job_posting_id = req.body.job_posting_id;
     const comment = req.body.comment;
     const issue_type = req.body.issue_type;
+    const is_resolved = req.body.is_resolved;
+    const issues_email = req.body.issues_email;
     // the query that's responsible for inserting job issues to the database
     // this query will probably need a join with the job_postings table
-    const queryText = `INSERT INTO "issues" ("comment", "issue_type")
-    VALUES ($1, $2) RETURNING id;`;
+    const queryText = `INSERT INTO "issues" ("job_posting_id", "comment", "issue_type", "is_resolved", "issues_email")
+    VALUES ($1, $2, $3, $4, $5) RETURNING id;`;
     // this pools the query text and the datafields and sends the data on to the database
     pool
-        .query(queryText, [comment, issue_type])
+        .query(queryText, [job_posting_id, comment, issue_type, is_resolved, issues_email])
         .then(() => res.sendStatus(201))
         .catch((err) => {
             console.log('job issue POST failed: ', err);
