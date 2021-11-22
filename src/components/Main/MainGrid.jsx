f446b089a5d6ab95970e4f2175e0fd0668e5403b
Greg Tambornino <gregtambornino@gmail.com>
2021-11-22 12:46:26 -0600

    importing history push

--- src/components/Main/MainGrid.jsx
+++ src/components/Main/MainGrid.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import { Button } from '@mui/material';
 import { format, compareAsc, parseISO } from 'date-fns';
