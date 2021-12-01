f4b69619dc3bd149d9b1b5780b9744a9e4a1f852
Danny Mussie <dannymussie7@gmail.com>
2021-11-30 19:24:06 -0600

    edited main grid and remote jobs grid, fixed typos and adjusted field widths

--- src/components/Main/MainGrid.jsx
+++ src/components/Main/MainGrid.jsx
@@ -25,12 +25,12 @@ function MainGrid ({ rows }) {
 
     // The application field column will render a button that will take user to link wether the employer has put in a 'https' or not.
     const columns = [
-        { field: 'company_name', headerName: 'company', width: 150 },
-        { field: 'date_posted', headerName: 'date', width: 110, renderCell: (params) => {return(moment(params.row.date_posted).format('MMM Do YY'))} },
-        { field: 'available_role', headerName: 'available role', width: 150 },
-        { field: 'description', headerName: 'description', width: 150 },
+        { field: 'company_name', headerName: 'Company', width: 150 },
+        { field: 'date_posted', headerName: 'Date', width: 110, renderCell: (params) => {return(moment(params.row.date_posted).format('MMM Do YY'))} },
+        { field: 'available_role', headerName: 'Available Role', width: 175 },
+        { field: 'description', headerName: 'Description', width: 200 },
         {
-            field: 'application_link', headerName: 'link', width: 150, renderCell: (params) => {
+            field: 'application_link', headerName: 'Link', width: 150, renderCell: (params) => {
                 if (params.row.application_link.includes('http')) {
                     return <Button style={{ backgroundColor: '#E7F2F8' }} variant="contained"><a style={{ color: 'black', fontWeight: '500' }} href={`${params.row.application_link}`} target="_blank">Apply</a></Button>
                 } else {
@@ -38,9 +38,9 @@ function MainGrid ({ rows }) {
                 }
             }
         },
-        { field: 'job_type', headName: 'Job Field', width: 350 },
+        { field: 'job_type', headerName: 'Job Type', width: 350 },
         {
-            field: 'id', headerName: 'Any Issues?', width: 150, renderCell: (params) => {
+            field: 'id', headerName: 'Any Issues?', width: 160, renderCell: (params) => {
                 return <Button variant="contained" style={{ backgroundColor: '#FFA384', color: 'white', fontWeight: '600', padding: '0 6px' }} size="small"
                     onClick={() => toIssuePage(params.row.id)}> Report Issue </Button>
             }
