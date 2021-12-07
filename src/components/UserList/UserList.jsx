16016715bfcff35f4acd7a9d8d7b13e2eb087efc
Greg Tambornino <gregtambornino@gmail.com>
2021-12-07 11:54:27 -0600

    set up UserList on client to dispatch access updates

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import UserListItem from "./UserListItem";
 
 function UserList () {
     const userList = useSelector(store => store.userList);
@@ -16,17 +17,7 @@ function UserList () {
             <ul>
                 {userList.map((user) => {return(
                         
-                        <li key={user.id}>
-                            Username: {user.email} &nbsp;|&nbsp; 
-                            Full Name: {user.first_name} {user.last_name} &nbsp;|&nbsp;
-                            Access Level: {user.access_level} &nbsp;&nbsp;
-                            {(user.access_level < 1) ? (
-                                    <button onClick={} >Grant Admin Access</button>
-                                ) : (
-                                    <button onClick={} >Remove Admin Access</button>
-                                )}
-
-                        </li>
+                        <UserListItem user={user}/>
                         
                     )}
                 )}
---
+++ src/components/UserList/UserListItem.jsx
@@ -0,0 +1,38 @@
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+function UserListItem({user}) {
+    // set useDispatch for click event functions
+    const dispatch = useDispatch();
+    // set 
+
+    const handleGrantAccess = () => {
+        // dispatch something
+        console.log('Clicked grant access, user id:', user.id);
+        dispatch({ type: 'GRANT_ADMIN_ACCESS' });
+    }
+
+    const handleRemoveAccess = () => {
+        // dispatch something
+        console.log('Clicked Remove Access, user id:', user.id);
+        dispatch({ type: 'REMOVE_ADMIN_ACCESS' });
+    }
+
+    return(
+        <>
+            <li key={user.id}>
+                            Username: {user.email} &nbsp;|&nbsp; 
+                            Full Name: {user.first_name} {user.last_name} &nbsp;|&nbsp;
+                            Access Level: {user.access_level} &nbsp;&nbsp;
+                            {(user.access_level < 1) ? (
+                                    <button onClick={handleGrantAccess} >Grant Admin Access</button>
+                                ) : (
+                                    <button onClick={handleRemoveAccess} >Remove Admin Access</button>
+                                )}
+
+                        </li>
+        </>
+    )
+}
+
+export default UserListItem;
