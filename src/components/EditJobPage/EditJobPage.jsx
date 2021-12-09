11ae988582ecd89fe3e695d31c7cfc52d31bf3e5
Danny Mussie <dannymussie7@gmail.com>
2021-12-08 23:21:45 -0600

    cleanup code in EditJobPage.jsx

--- src/components/EditJobPage/EditJobPage.jsx
+++ src/components/EditJobPage/EditJobPage.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useHistory, useParams } from 'react-router-dom';
 import './EditJobPage.css';
 import EditJobForm from "./EditJobForm";
@@ -19,10 +19,6 @@ function EditJobPage() {
     const { id } = useParams();
     const jobToEdit = useSelector(store => store.setJobsReducer);
     const jobTypes = useSelector(store => store.jobTypes);
-    
-    
-    // set items in state     
-    // const [isLoading, setIsLoading] = useState(true);
     const showForm = (jobToEdit.length > 0) ? true : false;
     const thisJob = jobToEdit[0];
     
@@ -30,7 +26,6 @@ function EditJobPage() {
     useEffect( ()=> {
         dispatch({ type: 'GET_JOB_TYPES' });
         dispatch({ type: 'FETCH_JOB_ID', payload: { job_posting_id: id } });
-        // setIsLoading(false);
     }, []);
 
  
@@ -44,15 +39,12 @@ function EditJobPage() {
         <div>
             <h2>Editing this posting:</h2>
             <Button onClick={toAdminHub}>Back to List</Button>
-            {/* <p>{JSON.stringify(jobToEdit)}</p> */}
-            {/* <p>{JSON.stringify(jobTypes)}</p> */}
             {!showForm && <CircularProgress />}
             {showForm && 
             <div className="form-container">
             
                 <EditJobForm id={id} thisJob={thisJob} jobTypes={jobTypes} />
                 <Button onClick={toAdminHub}>Back to List</Button>
-                {/* <Button variant="contained" extended onClick={() => dispatch({ type: 'POST_APPROVED_JOBS'})}>Post approved jobs</Button> */}
             </div>}
         </div>
     )
